bfc34a03cbf1aabfe029ba6ddf56f4bee8178dca
Minjae Kim <minjae.kim@mytrum.com>
2022-05-20 00:02:30 +0900

    feat: proto interface 모듈 생성

---
+++ proto-interface/src/main/java/com/protointerface/ProtoInterfaceApplication.java
@@ -0,0 +1,14 @@
+package com.protointerface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoInterfaceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProtoInterfaceApplication.class, args);
+    }
+
+}
---
+++ proto-interface/src/test/java/com/protointerface/ProtoInterfaceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.protointerface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
